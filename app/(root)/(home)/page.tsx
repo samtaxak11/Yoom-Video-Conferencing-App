5e240deba24e7bdf88ec171905a344ddeacff50b
Sourabh Takshak <takshaksourabh11@gmail.com>
2024-08-02 18:29:09 +0530

    🎨Home component UI completed

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
+import MeetingTypeList from '@/components/MeetingTypeList';
+
 const Home = () => {
+  const now = new Date();
+
+  const time = now.toLocaleTimeString('en-US', {hour:'2-digit', minute:'2-digit'});
+  const date = (new Intl.DateTimeFormat('en-US', {dateStyle: 'full'})).format(now);
+
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className='text-3xl font-bold'>
-        Home
-      </h1>
+      <div className="h-[250px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h3 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">Upcoming Meeting at: 12:30 PM</h3>
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-4xl font-extrabold lg:text-4xl'>
+              {time}
+            </h1>
+            <p className="text-lg font-medium text-sky-1 lg:text-xl">
+              {date}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <MeetingTypeList />
     </section>
   )
 }
